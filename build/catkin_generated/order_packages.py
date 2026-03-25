f6eb5dd3316d17f341de82fc0d58ae4d55857a14
김동영 <146813532+kdy45@users.noreply.github.com>
2026-03-26 00:43:52 +0900

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/alpha08/models_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/alpha08/ros_edu_ws/devel;/opt/ros/noetic'.split(';') if '/home/alpha08/ros_edu_ws/devel;/opt/ros/noetic' != '' else []
