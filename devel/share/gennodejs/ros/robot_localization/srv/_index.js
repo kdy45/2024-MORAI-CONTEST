81aeb7eb60328b80898b53214f2e802351a43eae
김동영 <146813532+kdy45@users.noreply.github.com>
2026-03-26 00:30:15 +0900

    Add files via upload

---
+++ devel/share/gennodejs/ros/robot_localization/srv/_index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+let SetDatum = require('./SetDatum.js')
+let SetUTMZone = require('./SetUTMZone.js')
+let ToggleFilterProcessing = require('./ToggleFilterProcessing.js')
+let FromLL = require('./FromLL.js')
+let GetState = require('./GetState.js')
+let SetPose = require('./SetPose.js')
+let ToLL = require('./ToLL.js')
+
+module.exports = {
+  SetDatum: SetDatum,
+  SetUTMZone: SetUTMZone,
+  ToggleFilterProcessing: ToggleFilterProcessing,
+  FromLL: FromLL,
+  GetState: GetState,
+  SetPose: SetPose,
+  ToLL: ToLL,
+};
---
+++ devel/share/gennodejs/ros/smarp_msgs/msg/_index.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+let camInfo = require('./camInfo.js');
+let objectStatus = require('./objectStatus.js');
+let lidarStatus = require('./lidarStatus.js');
+let objInfo = require('./objInfo.js');
+let recogObj = require('./recogObj.js');
+
+module.exports = {
+  camInfo: camInfo,
+  objectStatus: objectStatus,
+  lidarStatus: lidarStatus,
+  objInfo: objInfo,
+  recogObj: recogObj,
+};
